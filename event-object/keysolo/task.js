efbc30ea28c2d11aa27eee265f126d1c64852a40
yulti879 <162827622+yulti879@users.noreply.github.com>
2024-09-29 20:13:16 +0300

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      if (event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
