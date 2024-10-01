1844d0e490c31503be2dfe98e7e831685aa082e4
yulti879 <162827622+yulti879@users.noreply.github.com>
2024-10-01 23:16:16 +0300

    Update1 task.js

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,14 @@
 const rotators = document.querySelectorAll('.rotator');
-const cases = document.querySelectorAll('.rotator__case');
 
-const arr = Array.from(cases);
+rotators.forEach(rotator => {
+	const cases = document.querySelectorAll('.rotator__case');
+	const arr = Array.from(cases);
 
-let i = 0;
+	let i = 0;
 
-
-setInterval(function() {
-	arr[i].classList.remove('rotator__case_active');
-	i == (arr.length - 1) ? i = 0 : i++;
-	arr[i].classList.add('rotator__case_active');
-}, 1000);
+	setInterval(function() {
+		arr[i].classList.remove('rotator__case_active');
+		i == (arr.length - 1) ? i = 0 : i++;
+		arr[i].classList.add('rotator__case_active');
+	}, 1000);
+})
